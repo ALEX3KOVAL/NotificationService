e1197de971caa11b7cca3c588c0edb741fa9db77
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-07 19:29:43 +0300

    master: retry bean.

--- server/src/main/java/ru/alex3koval/notificationService/server/configuration/MainConfiguration.java
+++ server/src/main/java/ru/alex3koval/notificationService/server/configuration/MainConfiguration.java
@@ -1,5 +1,9 @@
 package ru.alex3koval.notificationService.server.configuration;
 
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,4 +13,9 @@ import ru.alex3koval.notificationService.configuration.di.AppCommonConfiguration
 @Import({AppCommonConfiguration.class, HttpBodyDeserializerConfiguration.class})
 @ComponentScan("ru.alex3koval.notificationService.storage")
 public class MainConfiguration {
+    @Bean
+    @Qualifier("otpRetry")
+    Retry otpRetry(RetryRegistry retryRegistry) {
+        return retryRegistry.retry("otpRetry");
+    }
 }
